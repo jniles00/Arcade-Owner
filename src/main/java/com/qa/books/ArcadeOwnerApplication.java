4def113628dffe3ab0a7f14dc1d8d479211bfc6b
jorden-niles <jordenniles@gmail.com>
2022-03-25 10:05:00 +0000

    first commit

---
+++ src/main/java/com/qa/books/ArcadeOwnerApplication.java
@@ -0,0 +1,13 @@
+package com.qa.books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcadeOwnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArcadeOwnerApplication.class, args);
+	}
+
+}
