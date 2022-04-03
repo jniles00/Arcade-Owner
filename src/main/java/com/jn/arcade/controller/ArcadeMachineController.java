9c13693e3e8adc40f5488ddaeec17b6a34104748
jorden-niles <jordenniles@gmail.com>
2022-04-03 10:15:05 +0100

    Finished controller class

--- src/main/java/com/jn/arcade/controller/ArcadeMachineController.java
+++ src/main/java/com/jn/arcade/controller/ArcadeMachineController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -42,5 +43,14 @@ public class ArcadeMachineController {
 	@PutMapping("/update/{id}")
 	public ResponseEntity<ArcadeMachine> updateArcadeMachine(@PathVariable long id, @RequestBody ArcadeMachine arcadeMachine){
 		return new ResponseEntity<ArcadeMachine>(this.service.update(id, arcadeMachine), HttpStatus.ACCEPTED);
+	}
+	
+	// Deletes the information for an ArcadeMachine object by find its ID
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<Boolean> deleteArcadeMachine(@PathVariable long id){
+		// If the ArcadeMachine object is successfully deleted, it should return NO_CONTENT
+		return(this.service.delete(id)) ? new ResponseEntity<Boolean>(HttpStatus.NO_CONTENT):
+			// Else if it was not successful then it will return "NOT_FOUND"
+			new ResponseEntity<Boolean>(HttpStatus.NOT_FOUND);
 	}
 }
